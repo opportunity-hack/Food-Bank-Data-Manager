e01a32a7454bf1a4c48e7871ccb1d0942253ca69
MArk O <mark@markomo.me>
2018-10-21 00:27:28 -0700

    Added Excel export

--- FBM Utility/GenerateMonthlyReport.py
+++ FBM Utility/GenerateMonthlyReport.py
@@ -1,3 +1,4 @@
+import sys
 import calendar
 import datetime
 
@@ -6,6 +7,8 @@ import numpy as np
 import openpyxl
 
 from FoodBankManager import FBM
+
+donor_catagories = ["Grocery", "Org/Corp", "Individual", "Church", "Purchased", "Senior program"]
 
 def FindLastMonthsDates():
 	"""
@@ -52,7 +55,6 @@ def MonthlyGuestData(FBMInst,month=None, year=None):
 	return data
 
 def WriteSummeryData(q, ws, origin=(1,1),  month=None, year=None):
-	donor_catagories = ["Grocery", "Org/Corp", "Individual", "Church", "Purchased", "Senior program"]
 	if month is not None and year is not None:
 		start = datetime.date(year, month, 1)
 		end = datetime.date(year, month, calendar.monthrange(year, month)[1])
@@ -64,7 +66,7 @@ def WriteSummeryData(q, ws, origin=(1,1),  month=None, year=None):
 
 	ws.column_dimensions[ws.cell(row=origin[0], column=origin[1]).column].width = 14
 
-	for cell_row in range(origin[0], origin[0] + len(donor_catagories) + 3 + 1):
+	for cell_row in range(origin[0], origin[0] + len(donor_catagories) + 10 + 1):
 		ws.cell(row=cell_row, column=origin[1]).style = "Input"
 
 	ws.cell(row=origin[0], column=origin[1]).value = "{}".format(start.strftime("%B %Y"))
@@ -72,25 +74,40 @@ def WriteSummeryData(q, ws, origin=(1,1),  month=None, year=None):
 	ws.cell(row=origin[0], column=origin[1]).alignment = openpyxl.styles.Alignment(horizontal='center')
 	for i, item in enumerate(donor_catagories):
 		ws.cell(row=origin[0] + i + 1, column=origin[1]).value = donation_data[donation_data["DonorCategory"] == item].sum()["Weight (lbs)"]
-	ws.cell(row=origin[0] + len(donor_catagories) + 1, column=origin[1]).value = "=SUM({}:{})".format(ws.cell(row=origin[0] + 2, column=origin[1] + 1).coordinate, ws.cell(row=origin[0] + len(donor_catagories) + 1, column=origin[1] + 1).coordinate)
+	ws.cell(row=origin[0] + len(donor_catagories) + 1, column=origin[1]).value = "=SUM({}:{})".format(ws.cell(row=origin[0] + 1, column=origin[1]).coordinate, ws.cell(row=origin[0] + len(donor_catagories), column=origin[1]).coordinate)
 	ws.cell(row=origin[0] + len(donor_catagories) + 1, column=origin[1]).style = "Calculation"
 	ws.cell(row=origin[0] + len(donor_catagories) + 2, column=origin[1]).value = donation_data[donation_data["DonorCategory"] == "Waste"].sum()["Weight (lbs)"]
-	ws.cell(row=origin[0] + len(donor_catagories) + 3, column=origin[1]).value = "={}-{}".format(ws.cell(row=origin[0] + len(donor_catagories) + 2, column=origin[1] + 1).coordinate, ws.cell(row=origin[0] + len(donor_catagories) + 3, column=origin[1] + 1).coordinate)
+	ws.cell(row=origin[0] + len(donor_catagories) + 3, column=origin[1]).value = "={}-{}".format(ws.cell(row=origin[0] + len(donor_catagories) + 1, column=origin[1]).coordinate, ws.cell(row=origin[0] + len(donor_catagories) + 2, column=origin[1]).coordinate)
 	ws.cell(row=origin[0] + len(donor_catagories) + 3, column=origin[1]).style = "Calculation"
+	ws.cell(row=origin[0] + len(donor_catagories) + 4, column=origin[1]).style = "Normal"
+	ws.cell(row=origin[0] + len(donor_catagories) + 5, column=origin[1]).value = len(user_data.index)
+
+	ws.cell(row=origin[0] + len(donor_catagories) + 7, column=origin[1]).value = user_data["Tracking Result"].sum()
+	ws.cell(row=origin[0] + len(donor_catagories) + 8, column=origin[1]).value = "={}*40".format(ws.cell(row=origin[0] + len(donor_catagories) + 5, column=origin[1]).coordinate)
+	ws.cell(row=origin[0] + len(donor_catagories) + 9, column=origin[1]).style = "Normal"
+	ws.cell(row=origin[0] + len(donor_catagories) + 10, column=origin[1]).value = "=IF(ISTEXT({0:}), {2:}-{1:}, {2:}-{1:}+{0:})".format(ws.cell(row=origin[0] + len(donor_catagories) + 10, column=origin[1]-1).coordinate, ws.cell(row=origin[0] + len(donor_catagories) + 8, column=origin[1]).coordinate, ws.cell(row=origin[0] + len(donor_catagories) + 3, column=origin[1]).coordinate)
+
+
 
 def WriteSummeryLabel(ws, origin=(1,1)):
-	ws.column_dimensions[ws.cell(row=origin[0], column=origin[1]).column].width = 17
-	donor_catagories = ["Grocery", "Org/Corp", "Individual", "Church", "Purchased", "Senior program"]
+	ws.column_dimensions[ws.cell(row=origin[0], column=origin[1]).column].width = 22
 	for i, item in enumerate(donor_catagories):
-		ws.cell(row=origin[0] + i + 1, column=origin[1]).value = item
-	ws.cell(row=origin[0] + len(donor_catagories) + 1, column=origin[1]).value = "Total Food Income"
-	ws.cell(row=origin[0] + len(donor_catagories) + 2, column=origin[1]).value = "Waste"
-	ws.cell(row=origin[0] + len(donor_catagories) + 3, column=origin[1]).value = "Total"
+		ws.cell(row=origin[0] + i + 1, column=origin[1]).value = "{} (lbs)".format(item)
+	ws.cell(row=origin[0] + len(donor_catagories) + 1, column=origin[1]).value = "Total Food Income (lbs)"
+	ws.cell(row=origin[0] + len(donor_catagories) + 2, column=origin[1]).value = "Waste (lbs)"
+	ws.cell(row=origin[0] + len(donor_catagories) + 3, column=origin[1]).value = "Total Collected (lbs)"
 
-	for cell_row in range(origin[0], origin[0] + len(donor_catagories) + 3 + 1):
+	ws.cell(row=origin[0] + len(donor_catagories) + 5, column=origin[1]).value = "Number of Clients"
+	ws.cell(row=origin[0] + len(donor_catagories) + 6, column=origin[1]).value = "New Clients"
+	ws.cell(row=origin[0] + len(donor_catagories) + 7, column=origin[1]).value = "Total Impact"
+	ws.cell(row=origin[0] + len(donor_catagories) + 8, column=origin[1]).value = "Food Distributed (lbs)"
+
+	ws.cell(row=origin[0] + len(donor_catagories) + 10, column=origin[1]).value = "Ending Inventory (lbs)"
+
+	for cell_row in range(origin[0], origin[0] + len(donor_catagories) + 10 + 1):
 		ws.cell(row=cell_row, column=origin[1]).style = "Headline 4"
 
-def WriteExcelSheet(month=None, year=None):
+def WriteExcelSheet(name, month=None, year=None):
 	if month is not None and year is not None:
 		start = datetime.date(year, month, 1)
 		end = datetime.date(year, month, calendar.monthrange(year, month)[1])
@@ -102,13 +119,36 @@ def WriteExcelSheet(month=None, year=None):
 	user_data = MonthlyGuestData(q, month=start.month, year=start.year)
 
 	wb = openpyxl.Workbook()
-	ws = wb.active
+	sheet = wb.create_sheet("12 Month Overview")
+	wb.active = sheet
+	ws = sheet
+
 	WriteSummeryLabel(ws, origin=(2, 1))
-	for month in range(1, 12 + 1):
-		WriteSummeryData(q, ws, origin=(2, month+1), month=month, year=year)
-	wb.save("test.xlsx")
+	ym_start = 12 * year + month - 1
+	for i, ym in enumerate(range(ym_start - 11, ym_start + 1)):
+		y, m = divmod(ym, 12)
+		WriteSummeryData(q, ws, origin=(2, i+2), month=m+1, year=y)
+
+	ws.merge_cells('B1:M1')
+	for col in ['B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M']:
+		ws['{}1'.format(col)].style = "Headline 1"
+	ws['B1'] = "12 Month Overview"
+	ws['B1'].alignment = openpyxl.styles.Alignment(horizontal='center')
+
+	ws['N1'] = "Last Year's Preformance"
+	ws['N1'].style = "Headline 1"
+	WriteSummeryData(q, ws, origin=(2, 14), month=month, year=year-1)
+	ws.column_dimensions['N'].width = 30
+	ws['N18'].style = "Normal"
+	ws['N18'] = ""
+
+	ws.freeze_panes = "B3"
+
+	wb.save("{}.xlsx".format(name))
+	return "{}.xlsx".format(name)
 
 if __name__ == '__main__':
 	pd.set_option('display.expand_frame_repr', False)
-
-	print WriteExcelSheet(month=8, year=2018)
+	if len(sys.argv) < 3:
+		print "Run with \"<month number (1-12)> <Year (4 digit)>\""
+	print WriteExcelSheet("Report {}-{}".format(sys.argv[1], sys.argv[2]), month=int(sys.argv[1]), year=int(sys.argv[2]))
